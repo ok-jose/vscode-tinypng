621576dca2d9c3253a97010a18829d5a49b8397a
Jose Lee <lg282923166@gmail.com>
2019-09-01 19:23:05 +0800

    fix: remove .ts in ignore

---
+++ src/message/index.ts
@@ -0,0 +1,24 @@
+const message = {
+  en: {
+    compressing_tip: "Compressing file",
+    authentification_tip: "Authentification failed. Have you set the API Key?",
+    error_tip:
+      "Ooops, there is an error. Please check your source image and settings.",
+    not_available_tip: "TinyPNG API is currently not available.",
+    network_error_tip:
+      "Network issue occurred. Please check your internet connectivity.",
+    success_tip: "Image successfully compressed!",
+    api_tip:
+      "TinyPNG: API validation failed. Be sure that you filled out tinypng.apiKey setting already."
+  },
+  "zh-cn": {
+    compressing_tip: "正在压缩文件",
+    authentification_tip: "授权失败，请确定是否填写API key",
+    error_tip: "出错了, 请检查源图片文件和设置",
+    not_available_tip: "TinyPNG API暂时不可用",
+    network_error_tip: "网络错误",
+    success_tip: "图像压缩成功！",
+    api_tip: "TinyPNG: API验证失败，请确保你已经在设置里填写 tinypng.api"
+  }
+};
+export { message };
